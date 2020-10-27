ca1f4ec67525c1178f9b5941377de7373b64a851
ayciate@gmail.com <ayciate@gmail.com>
2020-10-27 13:48:26 +0000

    improvements

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # rawdraw-boids
- this is an quick and dirty implementation of the boids algorithm using cnlohr's rawdraw  
 
-
+this is an quick and dirty implementation of the boids algorithm using cnlohr's rawdraw
 
 there are 3 main rule that guides every boid
->seperation  
->alignment  
->cohesion  
 
-eventually goal seeking with be added for your mouse cursor but lets not get ahead of ourselves
+> seperation  
+> alignment  
+> cohesion
+
+eventually goal seeking with be added for your mouse cursor but lets not get ahead of ourselves  
+test test test
 
 whats working now
 >basic window with fps counter, window size and   
@@ -19,11 +20,11 @@ whats working now
 CNFGDrawRegPoly(short sides, short radius) is now a thing inside rawdraw, but is never used
 next
 >rotation of bird shape  DONE
->make birds start with nice spacing  
->point towards mouse
->smoothly point towards mouse
->get bird "moving" towards mouse
->get birds moving alone, randomly in the void
+>make birds start with nice spacing  ALT (place random, then place all evenly on the left side)
+>point towards mouse (in a function, not used/wonky)
+>smoothly point towards mouse (not needed)
+>get bird "moving" towards mouse > they move
+>get birds moving alone, randomly in the void > DONE
 >avoid crashing into edge  
 >bird sensing other birds  
 >bird sense with cone of vision (display this)   
@@ -42,6 +43,37 @@ Other todos
 
 maybe  
 >make engine steps not tied to frame rate  
+
+> basic window with fps counter, window size and  
+> can draw regular polygons  
+> can draw a bird-ish shape  
+> bird struct
+
+CNFGDrawRegPoly\(short sides, short radius\) is now a thing inside rawdraw, but is never used next
+
+> rotation of bird shape  
+> make birds start with nice spacing  
+> get bird "moving"  
+> avoid crashing into edge  
+> bird sensing other birds  
+> bird sense with cone of vision \(display this\)  
+> bird collision  
+> add sliders  
+> seperation  
+> alignment  
+> cohesion
+
+Other todos
+
+> clean up variable definitions  
+> reoder functions  
+> remove ununsed code
+> rawdraw webasm
+> add to gitpage
+
+maybe
+
+> make engine steps not tied to frame rate
 
 
 
--- boids.c
+++ boids.c
@@ -17,9 +17,9 @@
 //#include "boids.h"
 
 #define PI 3.14159265
-#define windowW 640
-#define windowH 360
-#define numberofBoids 300
+#define windowW 1920//1024
+#define windowH 1008//576
+#define numberofBoids 100
 unsigned frames = 0;
 unsigned long iframeno = 0;
 
@@ -27,16 +27,16 @@ int mouseX = 0;
 int mouseY = 0;
 int mouseMask = 0;
 int precision = 1;
-//double birbLayout[5][2] = {{0,-1},{-0.75,1},{0,0.25},{0.75,1},{0,-1}};
-double birbLayout[5][2] = {{0,1},{-0.75,-1},{0,-0.25},{0.75,-1},{0,1}};
+double birbLayout[5][2] = {{0,-1},{-0.75,1},{0,0.25},{0.75,1},{0,-1}};
+//double birbLayout[5][2] = {{0,1},{-0.75,-1},{0,-0.25},{0.75,-1},{0,1}};
 
 
 struct Boids{ 
     short size; // size mulitplyer for drawing
     int x;
     int y;
-    double rotation; // 0 to 1 works for me
-    double velocity; // probably will be unused as every step they'll pick a direction
+    int edgeScared;
+    double rotation; // radians 0 to 2*PI
     uint32_t boidColour;
 };
 
@@ -74,41 +74,94 @@ void HandleDestroy(){ // call to end the program
 void xyTransform(double * x, double * y, int sizeMultiplyer, double rotation){
     
     double newX, newY; // thank you cnlohr 
-
-    if(rotation){
-        newX = (*x * cos(rotation)) - (*y * sin(rotation));
-        newY = (*y * cos(rotation)) + (*x * sin(rotation));
-    } 
-    newX *= sizeMultiplyer;
-    newY *= sizeMultiplyer;
-
+    
+    // dont deal with wrap arround
+    if(rotation > 0){
+        //newX = (*x * cos(rotation)) - (*y * sin(rotation));
+        //newY = (*y * cos(rotation)) + (*x * sin(rotation));
+        newX *= sizeMultiplyer;
+        newY *= sizeMultiplyer;
+    } else {
+        newX = *x * sizeMultiplyer;
+        newY = *y * sizeMultiplyer;
+    }
     *x = newX;
     *y = newY;
 
 }
-// calculates true bearing
+
+// calculates true bearing of two points
+// radians
 double returnRotate(short x1, short y1, short x2, short y2){
     x2 = x2 - x1;
     y2 = y2 - y1;
     
     return atan2(x2, -y2);
-    
+}
 
+// return distance between two points
+double returnDistance(short x1, short y1, short x2, short y2){
+    double distance = 0; 
+    distance = sqrt(pow(x2-x1,2) + pow(y2-y1,2));
+    return distance;
+}
+
+// rotation to the opposite way
+// radians
+int returntoComfort(short x1, short y1, short x2, short y2){
+    // if uncomfortable
+    return returnRotate(0, 0, -(x2-x1), -(y2-y1)); 
 
 }
+
+int returnEdgeComfort(short x1, short y1, short w, short h, int comfortSize){
+    // if uncomfortable
+    if(x1-comfortSize < 0 || y1-comfortSize < 0 || x1+comfortSize > w || y1+comfortSize > h){
+        return 1;
+    }
+    return 0;
+}
+
+double mixRotation(double currentRotation, double rotateCohesion, double rotateAlignment, double rotateSeperation){
+
+
+    return (double)(currentRotation + rotateCohesion );
+}
+void drawRegPoly( int x, int y, int sides, int radius){
+
+    double seperation = (1/(double)sides)*(PI/0.5f);
+    double currentAngle = 0.0;
+    float currX;
+    float currY;
+    float prevX = 0.0f;
+    float prevY = 1.0f*(float)radius;
+
+    for(int i = 0; i <= sides; i++){
+        currX = sin(currentAngle)*radius;
+        currY = cos(currentAngle)*radius;;
+        CNFGTackSegment(x+prevX, y+prevY, x+currX, y+currY);
+        prevX = currX;
+        prevY = currY;
+
+        currentAngle += seperation;
+    }
+}
+
 // draws one boid
 // takes the number of the boid in the struct,(double) transforms the shape to its place
 // and then draws each segment (into int)
 void drawBirb(short birdNo){ // eventually make it so it can draw multiple points onto one thing
     double firstX, firstY, lastX, lastY;
     
+
+    // setting up first point
     firstX = (birbLayout[0][0]);
     firstY = (birbLayout[0][1]);
     // transform round 0,0
-    xyTransform(&firstX, &firstY, BoidStruct[birdNo].size, -BoidStruct[birdNo].rotation*2*PI);
+    xyTransform(&firstX, &firstY, BoidStruct[birdNo].size, -BoidStruct[birdNo].rotation);
     // translate to right space
-    firstX += BoidStruct[birdNo].x;
-    firstY += BoidStruct[birdNo].y;
+    firstX += (double)BoidStruct[birdNo].x;
+    firstY += (double)BoidStruct[birdNo].y;
     
     CNFGColor( BoidStruct[birdNo].boidColour );
     for(int i = 1; i <= 4; i +=1){
@@ -116,13 +169,13 @@ void drawBirb(short birdNo){ // eventually make it so it can draw multiple point
         // set second point
         lastX = (birbLayout[i][0]);
         lastY = (birbLayout[i][1]);
-        //printf("%f by %f\n", lastX, lastY);
+
         // transform it the same
-        xyTransform(&lastX, &lastY, BoidStruct[birdNo].size, -BoidStruct[birdNo].rotation*2*PI);
+        xyTransform(&lastX, &lastY, BoidStruct[birdNo].size, -BoidStruct[birdNo].rotation);
         // translate to the right space
         lastX += BoidStruct[birdNo].x;
         lastY += BoidStruct[birdNo].y;
-        //printf("%d by %d\n", lastX, lastY);
+        
         CNFGTackSegment(firstX,firstY,lastX,lastY);
 
         // prepare for next loop
@@ -138,18 +191,25 @@ void initBirbs(int w, int h){
     int boidCounter = 0; 
     int gridMultiplier = 100;
     int gridPadding = 75;
+    double birbSpacingH = (h - (2*gridPadding))/numberofBoids;
 
 
     // could be compressed into one loop but thats for later
     for(int i = 0; i < numberofBoids; i++){
-        //printf("init birb %d\n", i);
+        
+        // place boids left side + nicely spaced
+        //BoidStruct[i].x = gridPadding;    //(rand() % (w - gridPadding*2)) + gridPadding;
+        //BoidStruct[i].y = birbSpacingH * i + gridPadding;//(rand() % (h - gridPadding*2)) + gridPadding;
+
+        // place boids randomly 
         BoidStruct[i].x = (rand() % (w - gridPadding*2)) + gridPadding;
         BoidStruct[i].y = (rand() % (h - gridPadding*2)) + gridPadding;
 
         BoidStruct[i].size = 15;
-        BoidStruct[i].rotation = 0;//rand() / (double)(RAND_MAX)*2*PI;
+        BoidStruct[i].rotation = 0.0;
 
         BoidStruct[i].boidColour = rand();
+        
     }
     // i wanted to evenly space out the boids in a grid but i gave up...
     /*
@@ -175,67 +235,150 @@ void initBirbs(int w, int h){
 
 }
 
-void birbStep(short w, short h){ // one step forwards (should be called ~60 times a second)
-    int boidSpeed = 10;
+// one step forwards (should be called ~60 times a second)
+// this does a lot of things and is messy + unoptimised bc of all the loops, increasing number of boids will greatly decrease speed 
+void birbStep(short w, short h){ 
+    int boidSpeed = 1;
 
-    double wantToPoint = 0.0;
-    double rotateLimiter = 0.05; // ratio between current rotation and where it wants to point (maybe eventually can mix with collision)
+    // must be < number of boids
+    int considerationSize = 150; // how many of the nearest boids it considers to be worthwhile to calculate
+    // used for updating x, y
     double newX = 0;
     double newY = 0;
+
+    //rules 
     for(int i = 0; i < numberofBoids; i++){ 
         
         //init 
+        int totalX = 0 ;
+        int totalY = 0 ;
+        int totalLocal = 0;
+        
+        // final values that will make its way into the rotation
+        double rotateCohesion = 0.0;
+        double rotateAlignment = 0.0;
+        double rotateSeperation = 0.0;
+
+        int edgeComfortSize = 40; // space for the walls
+        int boidComfortSize = 40; // space between boids
+        int boidComfortCount = 0; // used to average out the emptiest direction
+        double totalBoidComfort = 0; // add it all here, average out into rotateSeperate
+        double totalRotation = 0.0;
+
+        double initialRotation = BoidStruct[i].rotation;
 
         //do stuff 
-        
-        // 3 steps
-        // collision 
+        CNFGColor( 0x777777);
+        drawRegPoly(BoidStruct[i].x,BoidStruct[i].y, 35, considerationSize);
+        // gather numbers for rules in local area 
+        for(int z = 0; z < numberofBoids; z++){
+            if(z != i){
+                // if in local space
+                if(returnDistance(BoidStruct[i].x, BoidStruct[i].y, BoidStruct[z].x, BoidStruct[z].y) < considerationSize){ 
+                    // points to a boid in it's local area
+                    CNFGColor( 0xffaaaa);
+                    CNFGTackSegment(BoidStruct[i].x, BoidStruct[i].y, BoidStruct[z].x, BoidStruct[z].y);
 
-        // where do i want to point
-        // calculate new direction 
-        BoidStruct[i].rotation = (BoidStruct[i].rotation + 0.01);
+                    // adding to local x/y average
+                    totalX += BoidStruct[z].x;
+                    totalY += BoidStruct[z].y;
+                    totalLocal++;
+                    // adding to local rotation average
+                    totalRotation += BoidStruct[z].rotation;
+
+                    // SEPERATION
+                    if(returnDistance(BoidStruct[i].x, BoidStruct[i].y, BoidStruct[z].x, BoidStruct[z].y) < boidComfortSize){   
+                        // show boundary has been crossed
+                        CNFGColor( 0xaaaaff);
+                        drawRegPoly(BoidStruct[z].x,BoidStruct[z].y, 15, boidComfortSize);
+
+                        // add to total
+                        totalBoidComfort += returntoComfort(BoidStruct[i].x, BoidStruct[i].y, BoidStruct[z].x, BoidStruct[z].y);
+                        
+                        // ++ to count
+                        boidComfortCount++;
+                    
+                    }
+                }
+            }
+        }
+
+        //////// Cohesion
+        rotateCohesion = returnRotate(BoidStruct[i].x, BoidStruct[i].y, totalX/(numberofBoids - 1), totalY/(numberofBoids - 1)); // minus one as we ignore the current boid
+
+        //////// Alignment
+        rotateAlignment = totalRotation / (numberofBoids - 1);
+
+        //////// Separation
+        // if colliding with others
+        if(totalBoidComfort) rotateSeperation = totalBoidComfort / (boidComfortCount); 
+
+        /*/ if near the edge (padding amount)
+        if(returnEdgeComfort(BoidStruct[i].x, BoidStruct[i].y, w, h, edgeComfortSize) || BoidStruct[i].edgeScared){
+            // rotate to the centre instead of using rules
+            if(BoidStruct[i].edgeScared <= 0) BoidStruct[i].edgeScared = 60;
+
+            BoidStruct[i].rotation = (BoidStruct[i].rotation*2 + returnRotate(BoidStruct[i].x, BoidStruct[i].y, w/2,h/2)/3);
+            BoidStruct[i].edgeScared--;
+        } else; {
+            // use rule rotation
+            
+        }*/
+        
+        // keep rotation within number bounds
+        //BoidStruct[i].rotation += 0.01;
+
+        if(totalBoidComfort){
+            BoidStruct[i].rotation = (rotateSeperation);
+
+        }else if (totalLocal > 4){
+
+            BoidStruct[i].rotation = (rotateAlignment);
+        }else{
+            BoidStruct[i].rotation = (rotateCohesion);
+        }
+
+        if(BoidStruct[i].rotation > 2*PI) BoidStruct[i].rotation = 0;
 
 
         // move in direction 
-        newX = BoidStruct[i].x + sin(BoidStruct[i].rotation*(PI/0.5))*boidSpeed;
-        newY = BoidStruct[i].y + cos(BoidStruct[i].rotation*(PI/0.5))*boidSpeed;
+        newX = sin(BoidStruct[i].rotation*2*PI)*boidSpeed;
+        newY = cos(BoidStruct[i].rotation*2*PI)*boidSpeed;
 
-        BoidStruct[i].x = newX;
-        BoidStruct[i].y = newY;
+        BoidStruct[i].x += newX;
+        BoidStruct[i].y += newY;
 
-        // keep it in bounds
-        if(BoidStruct[i].x < 0) BoidStruct[i].x = w;
-        if(BoidStruct[i].x > w) BoidStruct[i].x = 0;
+        char str[30];
+        sprintf(str, "%f", BoidStruct[i].rotation);
+        CNFGPenX=BoidStruct[i].x; CNFGPenY=BoidStruct[i].y;
+        CNFGDrawText(str, 3);
 
-        if(BoidStruct[i].y < 0) BoidStruct[i].y = h;
-        if(BoidStruct[i].y > h) BoidStruct[i].y = 0;
-        
-        // draw (we can do this after we step the bird forward as we wont be moving it again this frame)
-        drawBirb(i);
+        //BoidStruct[i].rotation += 0.001;
+        // walls are ENFORCED
+        if(1){
+            if(BoidStruct[i].x < 0) BoidStruct[i].x = 0;
+            if(BoidStruct[i].x > w) BoidStruct[i].x = w;
+
+            if(BoidStruct[i].y < 0) BoidStruct[i].y = 0;
+            if(BoidStruct[i].y > h) BoidStruct[i].y = h;
+
+        }
+        // looping walls (unused)
+        if(0){
+            if(BoidStruct[i].x < 0) BoidStruct[i].x = w;
+            if(BoidStruct[i].x > w) BoidStruct[i].x = 0;
+
+            if(BoidStruct[i].y < 0) BoidStruct[i].y = h;
+            if(BoidStruct[i].y > h) BoidStruct[i].y = 0;
+        }
+
 
     }
 }
-void drawRegPoly( int x, int y, int sides, int radius){
 
-    double seperation = (1/(double)sides)*(PI/0.5f);
-    double currentAngle = 0.0;
-    float currX;
-    float currY;
-    float prevX = 0.0f;
-    float prevY = 1.0f*(float)radius;
-
-    for(int i = 0; i <= sides; i++){
-        currX = sin(currentAngle)*radius;
-        currY = cos(currentAngle)*radius;;
-        CNFGTackSegment(x+prevX, y+prevY, x+currX, y+currY);
-        prevX = currX;
-        prevY = currY;
-
-        currentAngle += seperation;
-    }
-}
 int main()
 {
+    
     int i;
     double ThisTime;
 	double LastFPSTime = OGGetAbsoluteTime();
@@ -248,10 +391,11 @@ int main()
     char str[12];
     char str2[100];
 
+    // make random more random
     
-	CNFGSetup( "Boids Rawdraw Demo", 1920, 1008 );
-    // fullscreen because I dont want you to change the window size
-    //CNFGSetupFullscreen("Boids Rawdraw Demo", );
+    srand(time(NULL));
+	CNFGSetup( "Boids Rawdraw Demo", windowW, windowH );
+
     short w, h; // window size
     CNFGGetDimensions( &w, &h ); // updates the window size variables
     initBirbs(w,h);
@@ -300,14 +444,12 @@ int main()
 
         // draw hi
         CNFGPenX = stuffX; CNFGPenY = stuffY; 
-        //CNFGDrawBox( paddingAmount, paddingAmount, 0 + paddingAmount, 0 + paddingAmount );
-        CNFGColor( 0x000aff );
-        //drawRegPoly(stuffX, stuffY, round(w/100), 75);
+
         CNFGColor( rand() );
 
         // okay lets do some birbs 
         birbStep(w, h);
-        
+        for(int i = 0; i < numberofBoids; i++) drawBirb(i);
         CNFGColor( 0xffffff );
 
         // all base stuff finished, drawing the rest of the ui elements
@@ -340,13 +482,12 @@ int main()
 
 
 
-
+        
         ThisTime = OGGetAbsoluteTime();
 		if( ThisTime > LastFPSTime + 1 )
 		{
 			printf( "FPS: %d Mouse: %d,%d\n", frames, mouseX, mouseY);
             //printf("bird %d at %d-%d\n",0,BoidStruct[0].x,BoidStruct[0].y);
-
             LastFPS = frames; // for display
 			frames = 0;
 			linesegs = 0;
